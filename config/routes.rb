585c4bf25d236f969ed0b8b6e6f515f366a098ad
BEN <wahudong@email.com>
2019-10-09 00:46:00 -0500

    added about page and main menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  # get 'aboutme/index'
+  resources :aboutme, only: :index
+
+  root to: 'requests#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
